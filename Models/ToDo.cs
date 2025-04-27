b5b2276a966ecc81ac6eceef75f3bbc241d840b4
Noah Eckhardt <jawoll1909@gmail.com>
2025-04-27 22:03:09 +0200

    Done

---
+++ Models/ToDo.cs
@@ -0,0 +1,7 @@
+namespace ToDoList.Models;
+
+public class ToDo
+{
+    public string? Content { get; set; } 
+    public bool IsCompleted { get; set; } = false;
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,40 @@
-﻿namespace ToDoList.ViewModels;
+﻿using System.Collections.ObjectModel;
+using Avalonia.Media;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ToDoList.Models;
+
+namespace ToDoList.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty] private ObservableCollection<ToDo> _todoList = [];
+    
+    [ObservableProperty] private string? _newToDoContent;
+    public MainWindowViewModel()
+    {
+
+    }
+    
+    [RelayCommand]
+    private void AddToDo()
+    {
+        if (string.IsNullOrWhiteSpace(NewToDoContent))
+            return;
+
+        var newToDo = new ToDo
+        {
+            Content = NewToDoContent,
+            IsCompleted = false
+        };
+
+        TodoList.Add(newToDo);
+        NewToDoContent = string.Empty;
+    }
+    
+    [RelayCommand]
+    private void DeleteTodo(ToDo todo)
+    {
+        TodoList.Remove(todo);
+    }
 }
